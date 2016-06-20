5fb6159497ed1b5128d877d1aa711b51ae200a1c
root <you@example.com>
2016-06-19 21:51:25 -0300

    f

--- www/js/services/services.js
+++ www/js/services/services.js
@@ -32,8 +32,7 @@ angular.module('app.services', [])
 
 .factory('loadingService', function($ionicLoading){
 	loadingService = {};
-	loadingService.open = function(){$ionicLoading.show({	content: 'Loading',	animation: 'fade-in', showBackdrop: true,
-			maxWidth: 200,showDelay: 0	});	};
+	loadingService.open = function(){$ionicLoading.show({	content: 'Loading',	animation: 'fade-in', showBackdrop: true,	maxWidth: 200,showDelay: 0	});	};
 	loadingService.close = function(){	$ionicLoading.hide();	};
 	return loadingService;
 })
@@ -50,52 +49,29 @@ angular.module('app.services', [])
 
 		return urlevent;
 	};
-
 	eventService.createEvent = function(evento, $scope) {
-		var user = servicoAcad.pegarUsuarioSession();
-		var flag = eventService.agendados.find(user.IdUsuario,evento);
+		var user = servicoAcad.pegarUsuarioSession(); 	var flag = eventService.agendados.find(user.IdUsuario,evento);
 	     if(flag == null){
 			$cordovaCalendar.createEventInteractively({
-					title: evento.Titulo,
-					location: evento.Categoria.Nome,
-					notes: evento.Descricao,
-					startDate: new Date(evento.DataInicial),
-					endDate: new Date(evento.DataFinal)
+					title: evento.Titulo,	location: evento.Categoria.Nome, notes: evento.Descricao,	startDate: new Date(evento.DataInicial), endDate: new Date(evento.DataFinal)
 			}).then(function (result) {
-
-				 eventService.addAgendamento(evento);
-				 $scope.agendados =  eventService.quantidade();
-
-
+				 eventService.addAgendamento(evento);		 $scope.agendados =  eventService.quantidade();
 			}, function (err) {
 					console.error("There was an error: " + err);
 			});
     }
-		else {
-			toastService.show('Você já participa desse evento');
-		}
+		else {toastService.show('Você já participa desse evento');}
 	};
 var flagMsg = false;
 	eventService.alterar = function(eventonovo, $scope){
-
-       	var user = servicoAcad.pegarUsuarioSession();
-        var eventoantigo =eventService.agendados.eventoantigo(user.IdUsuario,eventonovo);
+       	var user = servicoAcad.pegarUsuarioSession();   var eventoantigo =eventService.agendados.eventoantigo(user.IdUsuario,eventonovo);
 				if(eventoantigo !== null)				{
-
 					$cordovaCalendar.modifyEvent({
-						title: eventoantigo.Titulo,
-						location: eventoantigo.Categoria.Nome,
-						notes: eventoantigo.Descricao,
-						startDate: new Date(eventoantigo.DataInicial),
-						endDate: new Date(eventoantigo.DataFinal),
-				    newTitle: eventonovo.Titulo,
-				    newLocation: eventonovo.Categoria.Nome,
-				    newNotes: eventonovo.Descricao,
-				    newStartDate: new Date(eventonovo.DataInicial),
-				    newEndDate: new Date(eventonovo.DataFinal)
+						title: eventoantigo.Titulo,	location: eventoantigo.Categoria.Nome, 	notes: eventoantigo.Descricao,	startDate: new Date(eventoantigo.DataInicial),	endDate: new Date(eventoantigo.DataFinal),
+				    newTitle: eventonovo.Titulo, newLocation: eventonovo.Categoria.Nome,  newNotes: eventonovo.Descricao, newStartDate: new Date(eventonovo.DataInicial), newEndDate: new Date(eventonovo.DataFinal)
 				  }).then(function (result) {
 						$scope.agendados =  eventService.quantidade();
-							flagMsg = true;
+						flagMsg = true;
 				  }, function (err) {
 				     console.log(err);
 				   });
@@ -105,51 +81,33 @@ var flagMsg = false;
 	eventService.deleteEvent = function(evento, $scope){
      if(eventService.removeAgendamento(evento, $scope)){
 				 $cordovaCalendar.deleteEvent({
-	 				title: evento.Titulo,
-	 				location: evento.Categoria.Nome,
-	 				notes: evento.Descricao,
-	 				startDate: new Date(evento.DataInicial),
-	 				endDate: new Date(evento.DataFinal)
+	 				title: evento.Titulo,	location: evento.Categoria.Nome, notes: evento.Descricao,	startDate: new Date(evento.DataInicial),endDate: new Date(evento.DataFinal)
 	 		  }).then(function (result) {
-	 		    toastService.show('Removido do seu calendário');
-					$scope.agendados =  eventService.quantidade();
-
-	 		  }, function (err) {
-	 		    console.log(err);
-	 		  });
+	 		    toastService.show('Removido do seu calendário');$scope.agendados =  eventService.quantidade();
+	 		  }, function (err) {  console.log(err);  });
 		 }
-		 else {
-			 toastService.show('Você ainda não participa desse evento');
-		 }
+		 else { toastService.show('Você ainda não participa desse evento'); }
 	};
-
 	eventService.removeAgendamento = function(evento){
     var retorno = false; 	var user = servicoAcad.pegarUsuarioSession();
-		if(user!==null){ retorno= eventService.agendados.remove(user.IdUsuario,evento);	}
+		if(user!==null){ retorno= eventService.agendados.remove(user.IdUsuario,evento);	};
 
 			return retorno;
 	};
-
 	eventService.addAgendamento=function(evento){
-		var retorno = false;
-		var user = servicoAcad.pegarUsuarioSession();
-		if(user !== null){ var agendamento= {IdUsuario:user.IdUsuario,evento}; retorno = eventService.agendados.add(agendamento);
-
-		}
+		var retorno = false;	var user = servicoAcad.pegarUsuarioSession();
+		if(user !== null){ var agendamento= {IdUsuario:user.IdUsuario,evento}; retorno = eventService.agendados.add(agendamento); };
 		return retorno;
 	};
-
 	eventService.findEvent = function(evento){
 		var retorno = 1 //btnExcluir abilitado
 		var user = servicoAcad.pegarUsuarioSession();
 		if(user !== null){
 			var agendamento = eventService.agendados.find(user.IdUsuario, evento);
-       if(agendamento ===null)
-			    retorno =0;
+       if(agendamento ===null)  retorno =0;
 		}
 		return retorno;
 	};
-
  eventService.quantidade = function(){
 	 var retorno = 0;	 var user = servicoAcad.pegarUsuarioSession();
 	 if(user !== null){ var agendamento = eventService.agendados.agendamentos(user.IdUsuario);if(agendamento !==null) retorno = agendamento;}
@@ -166,8 +124,7 @@ var flagMsg = false;
 	noticiaService = {};
 	noticiaService.url = function(){
 		var user = servicoAcad.pegarUsuarioSession();		var urlnoticia =  servicoAcad.urlBase +'noticia';
-		if(user != null)
-		  urlnoticia +='/'+user.IdUsuario+'?perfil='+user.PerfilUsuario;
+		if(user !== null)  urlnoticia +='/'+user.IdUsuario+'?perfil='+user.PerfilUsuario;
 		  return urlnoticia;
 	};
 	return noticiaService;
